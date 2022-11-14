142571503fa94876f0abbaa01cf4c0acfce2c78b
SwapnaMenda <115078741+SwapnaMenda@users.noreply.github.com>
2022-11-14 04:39:20 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+namespace ConnectingToSqlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=swapnaVM\mssqlserver02;Initial Catalog=master;Integrated Security=True";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            Console.WriteLine("Please Enter customer Id : ");
+            int cumId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nPlease Enter customer First Name : ");
+            String cumFName = Console.ReadLine();
+            Console.WriteLine("\nPlease Enter customer Last Name  : ");
+            String cumLname = Console.ReadLine();
+            Console.WriteLine("\n please Enter customer City : ");
+            String cumCity = Console.ReadLine();
+            Console.WriteLine("\nplease Enter customer Country:");
+            String cumCountry = Console.ReadLine();
+            Console.WriteLine("\nplease Enter customer Phone:");
+            String cumPhone = Console.ReadLine();
+
+           
+      
+            string insertQuery = "insert into customer values('" + cumId + "','" + cumFName + "','" + cumLname + "','"+cumCity+"','"+cumCountry+"','"+cumPhone+"')";
+            SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+           int rows= insertCommand.ExecuteNonQuery();
+            Console.WriteLine("Data Inserted");
+            sqlConnection.Close();
+        }
+    }
+}
+
---
+++ deletion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace delete
+{
+    class Deletion
+    {
+        static void Main()
+            
+        {
+            SqlConnection sqlConnection;
+            string connectionString = @"Data Source=swapnaVM\mssqlserver02;Initial Catalog=master;Integrated Security=True";
+             sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+            try
+            {
+                Console.WriteLine("enter the id value that you want to delete");
+                int Id = int.Parse(Console.ReadLine());
+
+                string deletequery = "delete from customer where Id=" + Id;
+                SqlCommand deleteCommand = new SqlCommand(deletequery, sqlConnection);
+
+                deleteCommand.ExecuteNonQuery();
+                Console.WriteLine("deleted data successfully");
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+            }
+        }
+    }
